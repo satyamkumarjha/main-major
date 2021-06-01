8f2b44d5a5252682309d0587d8c7d7a4329205f5
Satyam Kumar Jha <satyamjhakumar@gmail.com>
2021-06-01 16:04:49 +0530

    changed static

--- majorProject/settings.py
+++ majorProject/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,'static')
+#]
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
