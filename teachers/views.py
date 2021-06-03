b9ddbabb1d27bb20360503e58c46cfa5cf049f11
Satyam Kumar Jha <satyamjhakumar@gmail.com>
2021-06-03 12:19:41 +0530

    added front end and backend for teachers

--- teachers/views.py
+++ teachers/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from content.models import instructor,course
 
 # Create your views here.
 
@@ -6,4 +7,11 @@ def teacherView(request):
     loggen_in = False
     if request.user.is_authenticated:
         loggen_in = True
-    return render(request,'teachers.html',{'log':loggen_in, 'u':request.user})
+    course_list = [[] for i in range(instructor.objects.count())]
+    counter = 1
+    for i in instructor:
+        for c in course:
+            if c.instructor_name == i.instructor_name:
+                course_list[counter].append(c.course_name)
+        counter+=1
+    return render(request,'teachers.html',{'inst':instructor.objects.all(),'courses':course_list,'log':loggen_in, 'u':request.user})
