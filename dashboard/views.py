fd2be9e4c53495dab94960e1cc6406c90d952002
Satyam Kumar Jha <satyamjhakumar@gmail.com>
2021-06-03 21:39:51 +0530

    fixed quiz front end

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from typing import Mapping
 from django.shortcuts import render
 from .models import student_details,courses_enrolled
 from quiz.models import quiz
@@ -12,6 +13,7 @@ def view_dashboard(request):
     print(current_user)
     enrolled = []
     quizes = []
+    main_map = {}
     for course in courses_enrolled.objects.all():
         if course.username == current_user:
             enrolled.append(course)
@@ -19,10 +21,11 @@ def view_dashboard(request):
         print(q.course_name)
         for e in enrolled:
             if e.course_name == q.course_name:
+                main_map[q] = e
                 print('true')
                 quizes.append(q)
     for student in student_details.objects.all():
         if student.username == current_user:
             target_student = student
 
-    return render(request,'student-dashboard.html',{'student':target_student,'courses':enrolled, 'quizes' : quizes,'log':loggen_in, 'u':request.user})
+    return render(request,'student-dashboard.html',{'student':target_student,'map':main_map,'courses':enrolled, 'quizes' : quizes,'log':loggen_in, 'u':request.user})
